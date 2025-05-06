4a24ca38d8278997695e827b4f19131380ad482a
Elliott02JR <147387434+Elliott02JR@users.noreply.github.com>
2025-05-06 22:59:00 +0100

    Update Meal plan.py
    Added in a check to see if an ingredient had already been used and adds it if it hasnt. Also returns the Unique ID identifier if it hasnt so i can add it it the recipie table. Thsi will let me more easily in future connect different recipies and they will all use the same number system.

--- Meal plan.py
+++ Meal plan.py
@@ -6,9 +6,10 @@ class Shopping_list:
         self.connector = sqlite3.connect("Shopping List.db")
         self.cursor = self.connector.cursor()
 
+
     def Create_Tables(self,Recipie):
         try:
-            self.cursor.execute(f"CREATE TABLE {Recipie} (Ingredient char(20) PRIMARY KEY, quantity int NOT NULL)")
+            self.cursor.execute(f"CREATE TABLE {Recipie} (Ingredient TEXT PRIMARY KEY, Ing_ID int NOT NULL, quantity int NOT NULL)")
             return None
         except sqlite3.OperationalError:
             return ("Recipie Already Exists")
@@ -18,28 +19,46 @@ class Shopping_list:
     
     def Create_Ingredients_Table(self):
         try:
-            self.cursor.execute(f"CREATE TABLE Ingredients (Ing_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, Ingredient char(20) NOT NULL)")
+            self.cursor.execute(f"CREATE TABLE IF NOT EXISTS Ingredients (Ing_ID INTEGER PRIMARY KEY AUTOINCREMENT, Ingredient char(20) NOT NULL)")
+            print("Ingredients table created")
             return None
         except sqlite3.OperationalError:
             return ("Ingredients table already exists")
         except Exception as e:
-            return e
-        
-    def Ingredient_exist_check(self,,table, column, value):
+            #return e
+            print(f"OperationalError: {e}")
+
+    def Ingredient_exist_check(self,table, column, value):
 
         exist_check = f"SELECT EXISTS (SELECT 1 FROM {table} WHERE {column} = ?)"
         self.cursor.execute(exist_check,(value,))
         exists, = self.cursor.fetchone()
-        return bool(exists)
+        if bool(exists):
+            value_ID = f"SELECT Ing_ID FROM {table} WHERE {column} = ?"
+            self.cursor.execute(value_ID,(value,))
+            value_ID = self.cursor.fetchone()
+            return value_ID[0]
+        else:
+            add_ingredient = f"INSERT INTO {table} ({column}) VALUES (?)"
+            self.cursor.execute(add_ingredient,(value,))
+            self.connector.commit()
+            value_ID = f"SELECT Ing_ID FROM {table} WHERE {column} = ?"
+            self.cursor.execute(value_ID,(value,))
+            value_ID = self.cursor.fetchone()
+            return value_ID[0]
+
+                
 
     def add_recipie(self, Recipie):
         while True:
             print("Type Break into the console to stop the code running")
+            
             ing = input("Please Enter the Ingredient")
+            value_ID = self.Ingredient_exist_check(table = "Ingredients", column = "Ingredient", value = ing)
+            print(value_ID)
             if ing == "Break":
                 break
-            else:
-                
+            
 
                 
             quant = input("Please enter the quantity of " + ing)
@@ -47,20 +66,22 @@ class Shopping_list:
                 break
 
                 
-            add_recipie_row = f"INSERT INTO {Recipie}(Ingredient, quantity) VALUES (?,?)"
+            add_recipie_row = f"INSERT INTO {Recipie}(Ingredient, Ing_ID, quantity) VALUES (?,?,?)"
             try:
-                self.cursor.execute(add_recipie_row,(ing,quant))
-                self.connector.commit
+                self.cursor.execute(add_recipie_row,(ing,value_ID, quant))
+                self.connector.commit()
             except sqlite3.IntegrityError:
-                print("{ing} is already in list, please add another ingredient")
+                print(ing + " is already in list, please add another ingredient")
             except Exception as e:
-                print("Failed to add items")
+                print(f"OperationalError: {e}")
 
 
         
-
+Shopping_list().Create_Ingredients_Table()
 Shopping_list().Create_Tables(Recipie= "test")
+
 Shopping_list().add_recipie(Recipie="test")
+
 
 
 
